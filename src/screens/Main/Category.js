7f11dfb4e84e9d98139d4d9fe28d604bb7c72951
hyejin <study412@naver.com>
2021-08-28 22:24:22 +0900

    [feat] category list 추가

--- src/screens/Main/Category.js
+++ src/screens/Main/Category.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 
 const Category = ({ navigation }) => {
   const [count, setCount] = useState(1);
+  const [dummy, setDummy] = useState();
 
   useEffect(() => {
     console.log('hihi')
+
+    setDummy([
+        {
+            title: "hi",
+            user: "a",
+        },
+        {
+            title: "hoho",
+            user: "ho"
+        }
+    ])
   }, []);
 
   const plus = () => {
@@ -14,20 +26,42 @@ const Category = ({ navigation }) => {
   }
 
   function reset () {
-    navigation.navigate('Audio')
+    navigation.navigate('Home')
   }
+  
+const Styles = StyleSheet.create({
+    container: {
+        geight: 60,
+        felxDirection: 'row',
+        justifyContent : "space-around",
+        padding: 15,
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowOffset: {
+            width:0,
+            height:10,
+        },
+        shdowOpacity: 0.12,
+        shadowRadius: 5.46,
+        elevation:9,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+      },
+})
 
   return (
     <View>
-      <Text>오늘도 재미있는 이야기를 해주세요.</Text>
-      <Button onPress={reset} title="RESET"/>
-      <Text>category</Text>
-      {/* TODO: side scroll category */}
-      <FlatList>
-        <Button onPress={reset} title="RESET"/>
-        <Button onPress={reset} title="RESET"/>
-        <Button onPress={reset} title="RESET"/>
-      </FlatList>
+       
+      <View style = {Styles.container} >
+      <Button onPress={reset} title="return" />
+          <Text>카테고리</Text>
+      </View>
+        
+        <FlatList data = {dummy} 
+        renderItem = {({item}) => <Text style = {Styles.item}>{item.title}{item.user}</Text>}/>
     </View>
   )
 }
